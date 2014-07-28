2b1d2384c2548e9281a25dc1687dccbbe6415ec8
Priti <pritichandan11@gmail.com>
2014-07-28 11:39:07 +0530

    Implemented Bouncing Ball without Canvas(used css to draw ball)

--- Bouncing Ball/Ball.js
+++ Bouncing Ball/Ball.js
@@ -3,15 +3,18 @@ var dy=4;
 var y=150;
 var x=10;
 function draw()
-{
-	var a = document.getElementById("ball");
-	a.
-	if( x<0 || x>500)
-		dx=-dx;
-	if( y<0 || y>500)
-		dy=-dy;
+{	
+	var ball = document.getElementById("ball");
+	ball.style.position = "relative";
+	ball.style.top = y;
+	ball.style.left = x;
+	if( x<0 || x>450)
+			dx=-dx;
+	if( y<0 || y>450)
+			dy=-dy;
 	x+=dx;
 	y+=dy;
+	console.log(x);
+	console.log(y);
 }
-
 setInterval(draw,10); 
